551f880e59eda7e68f26783957bd53b2a02a4043
alvaro-ruiz <aaruizblanquez@gmail.com>
2023-02-03 14:00:03 +0100

    Primera clase persona

---
+++ Persona.java
@@ -0,0 +1,19 @@
+
+public class Persona {
+  
+  public String nombre;
+  public String apellidos;
+  public int edad;
+  
+  public Persona(String nombre, String apellidos, int edad) {
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.edad = edad;
+  }
+  
+  public void cumpleanios() {
+    edad++;
+    System.out.print(edad);
+  }
+
+}
